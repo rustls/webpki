a864a5ab4daeeb404595181cc626d57aca4c3950
Joseph Birr-Pixton <jpixton@gmail.com>
2023-08-30 10:26:46 +0100

    Add aws-lc-rs as optional dependency
    Export SignatureVerificationAlgorithms backed by it in webpki::aws_lc_rs

--- src/alg_tests.rs
+++ src/alg_tests.rs
@@ -12,6 +12,8 @@
 // ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 // OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
+#![allow(clippy::duplicate_mod)]
+
 use base64::{engine::general_purpose, Engine as _};
 
 use crate::error::{DerTypeId, Error};
