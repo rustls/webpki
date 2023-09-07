08fd43948dc11caf2684bc0f7cb15781585739f2
Daniel McCarney <daniel@binaryparadox.net>
2023-09-07 12:49:16 -0400

    verify_cert: take references in verify_chain helper
    This commit adjusts the arguments to the `verify_chain` test helper to
    take references instead of moving the arguments. This makes it easier to
    use the same inputs for multiple `verify_chain` invocations.

--- src/verify_cert.rs
+++ src/verify_cert.rs
@@ -620,7 +620,7 @@ mod tests {
             intermediates.pop();
         }
 
-        verify_chain(ca_cert_der, intermediates, make_end_entity(&issuer)).unwrap_err()
+        verify_chain(&ca_cert_der, &intermediates, &make_end_entity(&issuer)).unwrap_err()
     }
 
     #[test]
@@ -655,7 +655,7 @@ mod tests {
             issuer = intermediate;
         }
 
-        verify_chain(ca_cert_der, intermediates, make_end_entity(&issuer))
+        verify_chain(&ca_cert_der, &intermediates, &make_end_entity(&issuer))
     }
 
     #[test]
@@ -775,14 +775,14 @@ mod tests {
 
     #[cfg(feature = "alloc")]
     fn verify_chain(
-        trust_anchor_der: Vec<u8>,
-        intermediates_der: Vec<Vec<u8>>,
-        ee_cert_der: Vec<u8>,
+        trust_anchor_der: &[u8],
+        intermediates_der: &[Vec<u8>],
+        ee_cert_der: &[u8],
     ) -> Result<(), Error> {
         use crate::ECDSA_P256_SHA256;
         use crate::{EndEntityCert, Time};
 
-        let anchors = &[TrustAnchor::try_from_cert_der(&trust_anchor_der).unwrap()];
+        let anchors = &[TrustAnchor::try_from_cert_der(trust_anchor_der).unwrap()];
         let time = Time::from_seconds_since_unix_epoch(0x1fed_f00d);
         let cert = EndEntityCert::try_from(ee_cert_der).unwrap();
         let intermediates_der = intermediates_der
