holodorum/rustls-webpki

Add verification function that verifies signature with spki

src/lib.rs
@@ -57,6 +57,7 @@ mod end_entity;
 mod error;
 #[cfg(feature = "ring")]
 mod ring_algs;
+mod rpk_entity;
 mod signed_data;
 mod subject_name;
 mod time;
@@ -78,6 +79,7 @@ pub use {
     },
     end_entity::EndEntityCert,
     error::{DerTypeId, Error},
+    rpk_entity::RawPublicKeyCert,
     signed_data::alg_id,
     trust_anchor::anchor_from_trusted_cert,
     verify_cert::KeyUsage,

rustls/webpki

Co-authored-by: Adolfo Ochagavía <github@adolfo.ochagavia.nl>

src/lib.rs
@@ -57,6 +57,7 @@ mod end_entity;
 mod error;
 #[cfg(feature = "ring")]
 mod ring_algs;
+mod rpk_entity;
 mod signed_data;
 mod subject_name;
 mod time;
@@ -78,6 +79,7 @@ pub use {
     },
     end_entity::EndEntityCert,
     error::{DerTypeId, Error},
+    rpk_entity::RawPublicKeyEntity,
     signed_data::alg_id,
     trust_anchor::anchor_from_trusted_cert,
     verify_cert::KeyUsage,