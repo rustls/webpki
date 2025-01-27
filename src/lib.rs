739d98c9e1acef8bd39e8439143eccb6d292067f
Joe Birr-Pixton <jpixton@gmail.com>
2025-01-27 14:40:47 +0000

    Remove re-export of rustls-pki-types

--- src/lib.rs
+++ src/lib.rs
@@ -86,8 +86,6 @@ pub use {
     verify_cert::VerifiedPath,
 };
 
-pub use pki_types as types;
-
 #[cfg(feature = "alloc")]
 pub use crl::{OwnedCertRevocationList, OwnedRevokedCert};
 
@@ -121,7 +119,7 @@ pub mod aws_lc_rs {
 /// An array of all the verification algorithms exported by this crate.
 ///
 /// This will be empty if the crate is built without the `ring` and `aws-lc-rs` features.
-pub static ALL_VERIFICATION_ALGS: &[&dyn types::SignatureVerificationAlgorithm] = &[
+pub static ALL_VERIFICATION_ALGS: &[&dyn pki_types::SignatureVerificationAlgorithm] = &[
     #[cfg(feature = "ring")]
     ring::ECDSA_P256_SHA256,
     #[cfg(feature = "ring")]
